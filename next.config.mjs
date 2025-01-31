a613738f522826b887f668a37abaebf5b8c0b5f0
communications-aagnyasoft <communications@aagnyasoft.com>
2025-01-31 15:27:27 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ try {
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Add this for static export
+  basePath: '/aagnyasoft-ai-labs',
   eslint: {
     ignoreDuringBuilds: true,
   },
